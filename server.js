6a8fd1fc9ff20c8122a0c16217d3b8323c7725dc
Lucien <lucien.crn@gmail.com>
2019-02-14 12:28:44 +0100

    Une bonne base

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 //Imports
 
-
+    const express = require('express');
+    const body_parser = require('body-parser');
+    require('dotenv').config();
+    const ejs = require('ejs');
+    const path = require ('path');
 
 //Configuration
 
-
+    const serveur = express();
+    const port = process.env.PORT;
 
 //Démarrer le serv
 
+serveur.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`)
+});
